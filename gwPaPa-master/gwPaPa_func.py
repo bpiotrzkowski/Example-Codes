adbbdfa8529ce42e0d13b3640a1041f594adfa85
Brandon Piotrzkowski <brandonpiotrzkowski@Brandons-MacBook-Pro.local>
2019-01-14 14:29:41 -0600

    Fixed bug where noise was only ever added. Added bandpass filter and related graphs.

--- gwPaPa-master/gwPaPa_func.py
+++ gwPaPa-master/gwPaPa_func.py
@@ -1,6 +1,19 @@
 import numpy as np
+from scipy.signal import butter,lfilter
 
 def twindow(t,h,t0,tl): 
 	ind=(t0<t)==(t<(t0+tl))
 	return h[ind]
 
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
